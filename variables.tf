2251ba18c657fc1d529c2b58cc2f080e48e0491f
ratnakottara <140639468+ratnakottara@users.noreply.github.com>
2023-09-11 22:50:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "instance_ami" {
 
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket."
-  default     = "my_buckett_terraformm"
+  default     = "my-terraform-bucket010"
 }
 
 variable "security_group_ingress" {
