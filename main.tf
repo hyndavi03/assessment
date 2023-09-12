1e870f9061fb4f1f7cf3eae4fc8c2a66af3db1ae
ratnakottara <140639468+ratnakottara@users.noreply.github.com>
2023-09-12 22:43:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,6 +44,19 @@ resource "aws_security_group" "main" {
   tags = {
     Name = "MySG-1"
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   // Define your security group rules here
 }
